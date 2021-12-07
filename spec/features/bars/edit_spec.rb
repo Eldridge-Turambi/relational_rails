d8d3545ee00f232abdcf16cb0313d352cd743dd5
Mallory Vining <mallorygallery12@gmail.com>
2021-12-06 20:18:25 -0700

    Magic Commit

--- spec/features/bars/edit_spec.rb
+++ spec/features/bars/edit_spec.rb
@@ -1,20 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'bars index page' do
-  before :each do
-    @monkey_bar = Bar.create!(name: 'Monkey Bar', employee_count: 10, license: true)
-    @fort_greene = Bar.create!(name: 'Fort Greene', employee_count: 20, license: true)
-    @gin_mill = Bar.create!(name: 'Gin Mill', employee_count: 30, license: true)
-    @french_75 = Drink.create!(name: 'French 75', cost: 15, alcoholic_bev: true, bar_id: @fort_greene.id)
-    @margarita = Drink.create!(name: 'Margarita', cost: 10, alcoholic_bev: true, bar_id: @fort_greene.id)
-    @glühwein = Drink.create!(name: 'Glüwhein', cost: 5, alcoholic_bev: true, bar_id: @monkey_bar.id)
-  end
   it 'links to an update form' do
-    visit "/bars/#{@fort_greene.id}"
+    fort_greene = Bar.create!(name: 'Fort Greene', employee_count: 20, license: true)
+
+    visit "/bars/#{fort_greene.id}"
 
     click_link("Update Bar")
 
-    expect(current_path).to eq("/bars/#{@fort_greene.id}/edit")
+    expect(current_path).to eq("/bars/#{fort_greene.id}/edit")
   end
 
   it 'can create a new bar with attributes with a form' do
