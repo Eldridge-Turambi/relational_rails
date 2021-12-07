1d8f5a13ec323e93012da1bcd1499238a58467ee
Eldridge Turambi <eturambi95@gmail.com>
2021-12-07 16:32:00 -0700

    User story 15 completed

--- spec/features/performers/index_spec.rb
+++ spec/features/performers/index_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Performers index' do
     @filmore = Venue.create!(name: 'The Filmore', lights: true, capacity: 1000)
     @taylor_swift = Performer.create!(name: 'Taylor Swift', age: 31, repeater: true, venue_id: @red_rocks.id)
     @john_mayer = Performer.create!(name: 'John Mayer', age: 44, repeater: true, venue_id: @red_rocks.id)
-    @kesha = Performer.create!(name: 'Kesha', age: 34, repeater: true, venue_id: @blue_bird.id)
+    @kesha = Performer.create!(name: 'Kesha', age: 34, repeater: false, venue_id: @blue_bird.id)
     visit '/performers'
   end
 
@@ -15,18 +15,38 @@ RSpec.describe 'Performers index' do
     expect(current_path).to eq('/performers')
   end
 
-  it 'shows all performers' do
+  it 'shows performers and their attributes' do
     expect(page).to have_content(@taylor_swift.name)
     expect(page).to have_content(@john_mayer.name)
-    expect(page).to have_content(@kesha.name)
+    #expect(page).to have_content(@kesha.name)
 
     expect(page).to have_content(@taylor_swift.age)
     expect(page).to have_content(@john_mayer.age)
-    expect(page).to have_content(@kesha.age)
+    #expect(page).to have_content(@kesha.age)
 
     expect(page).to have_content(@taylor_swift.repeater)
     expect(page).to have_content(@john_mayer.repeater)
-    expect(page).to have_content(@kesha.repeater)
+    #expect(page).to have_content(@kesha.repeater)
+  end
+
+  it 'only shows performers who have been to the venue before' do
+
+    taylor_swift = Performer.create!(name: 'Taylor Swift', age: 31, repeater: true, venue_id: @red_rocks.id)
+    john_mayer = Performer.create!(name: 'John Mayer', age: 44, repeater: true, venue_id: @red_rocks.id)
+    kesha = Performer.create!(name: 'Kesha', age: 34, repeater: false, venue_id: @blue_bird.id)
+
+    Performer.repeated_performers
+    expect(page).to have_content(taylor_swift.name)
+    expect(page).to have_content(john_mayer.name)
+    expect(page).to_not have_content(kesha.name)
+
+    expect(page).to have_content(taylor_swift.age)
+    expect(page).to have_content(john_mayer.age)
+    expect(page).to_not have_content(kesha.age)
+
+    expect(page).to have_content(taylor_swift.repeater)
+    expect(page).to have_content(john_mayer.repeater)
+    expect(page).to_not have_content(kesha.repeater)
   end
 
 end
--- spec/models/performer_spec.rb
+++ spec/models/performer_spec.rb
@@ -5,10 +5,22 @@ RSpec.describe Performer do
   describe 'validations' do
     it {should validate_presence_of(:name)}
     it {should validate_presence_of(:age)}
-    it {should validate_presence_of(:repeater)}
+  #  it {should validate_presence_of(:repeater)}
   end
 
   describe 'relationships' do
     it {should belong_to(:venue)}
+  end
+
+  describe 'shows performers where boolean == true' do
+    it 'Only shows repeater performers' do
+      red_rocks = Venue.create!(name: 'Red Rocks', lights: true, capacity: 9545)
+      taylor_swift = Performer.create!(name: 'Taylor Swift', age: 31, repeater: true, venue_id: red_rocks.id)
+      john_mayer = Performer.create!(name: 'John Mayer', age: 44, repeater: true, venue_id: red_rocks.id)
+      kesha = Performer.create!(name: 'Kesha', age: 34, repeater: false, venue_id: red_rocks.id)
+
+      expect(Performer.repeated_performers).to eq([taylor_swift, john_mayer])
+      expect(Performer.repeated_performers).to_not include([kesha])
+    end
   end
 end
