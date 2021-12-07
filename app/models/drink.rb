98326114c5f19a93305f2d9f8736739a5f2008fa
Mallory Vining <mallorygallery12@gmail.com>
2021-12-07 16:31:45 -0700

    Add User Srtory 15

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,7 +1,10 @@
 class Drink < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :cost
-  validates_presence_of :alcoholic_bev
 
   belongs_to :bar
+
+  def self.is_alcoholic
+    where(alcoholic_bev: true)
+  end
 end
